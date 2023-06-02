50db448a5f45b4fb4f1d6761d46fdd2efef199d5
unknown <jfr@obvious.tech>
2023-06-02 11:53:22 +0200

    Fixed Follow View

--- Assets/Scripts/FixedFollowView.cs
+++ Assets/Scripts/FixedFollowView.cs
@@ -4,7 +4,7 @@ namespace TPCamera
 {
     public class FixedFollowView : AView
     {
-        public Transform TargetConfiguration;
+        public Transform TargetTransform;
 
         public float Yaw;
         public float Pitch;
@@ -16,15 +16,51 @@ namespace TPCamera
         public float Fov;
 
         public GameObject centralPoint;
-        public float FawOffsetMax;
+        public float YawOffsetMax;
         public float PitchOffsetMax;
 
-        public override CameraConfiguration GetConfiguration() => new(Yaw, Pitch, Roll, 0, Fov, transform.position);
+        public override CameraConfiguration GetConfiguration() => new(CalculateYaw(), CalculatePitch(), Roll, 0, Fov, transform.position);
 
-        private void Awake()
+        private float CalculateYaw()
         {
-            Yaw = Mathf.Atan2((TargetConfiguration.position - transform.position).normalized.x, (TargetConfiguration.position - transform.position).normalized.z) * Mathf.Rad2Deg;
-            Pitch = -Mathf.Asin((TargetConfiguration.position - transform.position).normalized.y) * Mathf.Rad2Deg;
+            float centralYaw = Mathf.Atan2((centralPoint.transform.position - transform.position).normalized.x,
+                                                    (centralPoint.transform.position - transform.position).normalized.z) * Mathf.Rad2Deg;
+            float yaw = Mathf.Atan2((TargetTransform.position - transform.position).normalized.x,
+                                                    (TargetTransform.position - transform.position).normalized.z) * Mathf.Rad2Deg;
+            
+            float finalYaw = yaw - centralYaw;
+
+            while (finalYaw < 180)
+                finalYaw += 360;
+
+            while (finalYaw > 180)
+                finalYaw -= 360;
+
+            Yaw = finalYaw + centralYaw;
+            return Yaw;
+        }
+
+        private float CalculatePitch()
+        {
+            float centralPitch = -Mathf.Asin((centralPoint.transform.position - transform.position).normalized.y) * Mathf.Rad2Deg;
+            float pitch = -Mathf.Asin((TargetTransform.position - transform.position).normalized.y) * Mathf.Rad2Deg;
+
+            float finalPitch = pitch - centralPitch;
+            Mathf.Clamp(finalPitch, -PitchOffsetMax, PitchOffsetMax);
+
+            Pitch = finalPitch + centralPitch;
+            return Pitch;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position, 1.0f);
+            Vector3 position = transform.position;
+            Gizmos.DrawLine(transform.position, position);
+            Gizmos.matrix = Matrix4x4.TRS(position, Quaternion.Euler(Pitch, Yaw, Roll), Vector3.one);
+            Gizmos.DrawFrustum(Vector3.zero, Fov, 5.0f, 0f, Camera.main.aspect);
+            Gizmos.matrix = Matrix4x4.identity;
         }
     }
 }
